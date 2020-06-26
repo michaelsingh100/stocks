b378b98cc87ec4d779edc47db5ea5b46dc5b8e1c
michaelsingh100 <Michael@Michaels-MacBook-Pro-3.local>
2020-06-26 13:13:14 -0500

    String format error

--- corona/pullers/agg_mean.py
+++ corona/pullers/agg_mean.py
@@ -3,7 +3,7 @@ from ..models import Statistics
 
 class MeanPuller:
     def updatemean(self,start):
-        results = ClosingPoints.objects.raw("select symbol, avg(price) as mean, std(price) as stan where date > '%s 00:00:00' group by symbol")
+        results = ClosingPoints.objects.raw("select symbol, avg(price) as mean, std(price) as stan where date > '%s 00:00:00' group by symbol" % (start))
 
         enhan = (Statistics(symbol=result.symbol,mean_2020=result.mean,std_2020=result.stan) for result in results)
 
